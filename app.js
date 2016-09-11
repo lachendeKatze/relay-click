426439f968c8b13fbe339f8def8616cb2de6ef42
lachendeKatze <govkn@yahoo.com>
2016-09-11 15:05:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ annyang.addCommands(commands);
 annyang.start();
 
 var bleConnect = function(){
-    relayClick.connect()
+  console.log('connect');
+  /*  relayClick.connect()
     .then(() => console.log('connected'))
     .catch(error => { console.log('connect error!');
-    });
+    });*/
 };
 
 // maintain these functions to allow button & oice control options
