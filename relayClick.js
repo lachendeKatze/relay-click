630aff8a0323568b23551f5392430f7905fea483
lachendeKatze <govkn@yahoo.com>
2016-09-22 11:29:09 -0400

    comments

--- relayClick.js
+++ relayClick.js
@@ -3,12 +3,16 @@
 
   class RelayClick {
 
+    /**
+     * customize your project here to reflect the uuid of your service and characteristics.
+     */
     constructor() {
         this.deviceName = 'relays';
         this.serviceUUID = '917649a0-d98e-11e5-9eec-0002a5d5c51b';
         this.characteristic1UUID = '917649a1-d98e-11e5-9eec-0002a5d5c51b';
         this.device = null;
         this.server = null;
+        // The cache allows us to hold on to characeristics for access in response to user commands 
         this._characteristics = new Map();
     }
 
